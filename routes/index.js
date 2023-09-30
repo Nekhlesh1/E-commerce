9f55b938bd2b077304a1d1371dba4447633ac7cb
Nekhlesh SIngh <38405081+Nekhlesh1@users.noreply.github.com>
2023-10-01 00:57:32 +0530

    bug fixes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const productsController = require('../controllers/productsController');
 
 router.get('/product',productsController.products );
 router.post('/product/create', productsController.create);
+router.delete('/product/delete/:productID', productsController.delete);
+router.post('/product/:productID/update_quantity', productsController.updateQunatity)
 
 
 module.exports = router;
