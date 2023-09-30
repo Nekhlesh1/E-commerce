2398816aa1f7750a1467489467451113eba8afb6
Nekhlesh SIngh <38405081+Nekhlesh1@users.noreply.github.com>
2023-10-01 00:05:29 +0530

    final commit

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const port = 3001;
+const db = require('./config/mongoose');
+const bodyParser = require('body-parser');
+
+// INITIALIZING EXPRESS
 const app = express();
-const db = require('./config/mongoose')
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+//  SETTING ROUTER
 app.use('/', require('./routes/index'));
 
+
+//  CREATING SERVER
 app.listen(port, (err) => {
     if (err) {
         console.log("Error connecting to server!!", err);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const productsController = require('../controllers/productsController');
+
+router.get('/product',productsController.products );
+router.post('/product/create', productsController.create);
 
 
 module.exports = router;
